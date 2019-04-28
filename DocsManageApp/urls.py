485e25e45690683b1199672bda69910e3c71ea58
DONGWON KIM <lunacircle4@gmail.com>
2019-04-29 00:59:55 +0900

    비동기식 crud 기능 구현 완료

--- DocsManageApp/urls.py
+++ DocsManageApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 #urlpatterns는 없으면 에러 발생
 urlpatterns = [
     path('',views.readDocument, name='readDocument'),
-    path('createDocument',views.createDocument, name='createDocument'),
-    path('<int:pk>/updateDocument',views.updateDocument, name='updateDocument'),
-    path('<int:pk>/deleteDocument',views.deleteDocument, name='deleteDocument')
+    path('createDocument/',views.createDocument, name='createDocument'),
+    path('<int:pk>/updateDocument/',views.updateDocument, name='updateDocument'),
+    path('<int:pk>/deleteDocument/',views.deleteDocument, name='deleteDocument')
 ]
--- DocsManagementSystem/urls.py
+++ DocsManagementSystem/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('DocsManageApp/', include('DocsManageApp.urls')),
+    path('', include('DocsManageApp.urls')),
     path('admin/', admin.site.urls),
 ]
